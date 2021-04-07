c7ce0bcd40e62db30d79799d83c648cf33efe72d
Mainak Ghosh <contactwithmainak@gmail.com>
2021-04-07 13:51:48 +0530

    Again Initail Commit

--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
+// default route
 router.get("/", (request, response) => {
   response.send("Are you sure you wanna face this right now?");
 });
 
+// children routes
 router.get("/year2016", (request, response) => {
   response.send("What is this ? I'm gonna die for sure");
 });
