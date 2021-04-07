f11f705685dae137446b048066f3b3f134de7c8f
Mainak Ghosh <contactwithmainak@gmail.com>
2021-04-07 14:36:54 +0530

    Initial commit 3

--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,7 @@ router.get("/", (request, response) => {
   response.send("Are you sure you wanna face this right now?");
 });
 
-// children routes
+// children routes --
 router.get("/year2016", (request, response) => {
   response.send("What is this ? I'm gonna die for sure");
 });
